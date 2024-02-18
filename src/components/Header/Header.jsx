f3c88baa490a9608f74e9a6d02d1798e23d3c1cc
erangamdw <erangamdw@gmail.com>
2024-02-19 03:37:28 +0530

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,13 @@ import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
   return (
-    <div className="header flex flex-col" id="header">
+    <div className="flex flex-col header" id="header">
       <Navbar />
 
       <div className="container flex">
         <div className="header-content">
-          <h2 className="text-uppercase text-white op-07 fw-6 ls-2">
-            Health is your true ealth
+          <h2 className="text-white text-uppercase op-07 fw-6 ls-2">
+            Health is your true Wealth
           </h2>
           <h1 className="text-white fw-6 header-title">
             Welcome to <span className="text-brown">Arya Ayurveda</span>
@@ -20,7 +20,7 @@ const Header = () => {
           <h3 className="text-white fw-6">
             Care in the hands of True Ayurveda experts{" "}
           </h3>
-          <div className="btn-groups flex">
+          <div className="flex btn-groups">
             <button type="button" className="btn-item bg-brown fw-4 ls-2">
               Our Services
             </button>

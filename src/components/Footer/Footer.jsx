5c6ee0b78a74a7e2c09d6b42b46b86e08de5f2d0
erangamdw <erangamdw@gmail.com>
2024-02-28 22:49:52 +0530

    updated contact details

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ import {
 } from "react-icons/fa";
 
 const Footer = () => {
-  const phoneNumber = "+9412345678";
+  const phoneNumber = "+44 79 20107515";
   return (
     <div className="footer bg-dark section-p" id="footer">
       <div className="container">
@@ -41,15 +41,20 @@ const Footer = () => {
                 <FaPinterest />
               </a>
             </li>
-            <li>
-              <a href={`tel:${phoneNumber}`} className="text-white">
-                <FaPhone /> {phoneNumber}
-              </a>
-            </li>
+            <li className="phone-number">
+            <a href={`tel:${phoneNumber}`} className="text-white">
+              <FaPhone />
+              <span>{phoneNumber}</span>
+            </a>
+          </li>
           </ul>
           <p className="text-center text-white fs-14 op-07 fw-3 ls-1 reserved-text">
             All Rights Reserved &copy; 2024
           </p>
+        </div>
+        <div className="contact-info">
+          <p className="text-white">43A, Piercefield Road, Formby, L37 7DG</p>
+          <p className="text-white">Opening Hours: Monday to Sunday, 9am - 5pm</p>
         </div>
       </div>
     </div>
